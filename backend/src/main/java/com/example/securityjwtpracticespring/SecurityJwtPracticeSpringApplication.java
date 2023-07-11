62e62e196c718c23bb79960ec1de9bf4347812f7
kyoka-miy <kyoka.miyamura21@gmail.com>
2023-07-10 19:11:46 -0700

    first commit

---
+++ backend/src/main/java/com/example/securityjwtpracticespring/SecurityJwtPracticeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityjwtpracticespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtPracticeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtPracticeSpringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/securityjwtpracticespring/SecurityJwtPracticeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityjwtpracticespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtPracticeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
